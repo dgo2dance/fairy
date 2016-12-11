e8ca4e9b09220021ad4a6e9f74ba68b63af7608b
ShuYong Duan <duanshuyong0@163.com>
2016-12-11 20:46:05 +0800

    add item

--- fairy/items.py
+++ fairy/items.py
@@ -8,7 +8,7 @@
 
 from scrapy import Item, Field
 
-
+# 定义某个人的调仓历史
 class ChangeListItem(Item):
     """ 个人信息 """
     userId = Field()
@@ -19,3 +19,30 @@ class ChangeListItem(Item):
     target_weight = Field()  # 目标权重
     updated_at = Field()  # 交易时间
     stock_name = Field() #股票名字
+
+#定义最近三个月实盘大赛排名
+class QuarterRankingItem(Item):
+    name = Field()
+    symbol = Field()
+    rate = Field()
+    createTime = Field()
+    photo_domain = Field()
+    profile_image_url = Field()
+
+#定义最近一个月实盘大赛排名
+class MonthRankingItem(Item):
+    name = Field()
+    symbol = Field()
+    rate = Field()
+    createTime = Field()
+    photo_domain = Field()
+    profile_image_url = Field()
+
+#定义最近一周实盘大赛排名
+class WeekRankingItem(Item):
+    name = Field()
+    symbol = Field()
+    rate = Field()
+    createTime = Field()
+    photo_domain = Field()
+    profile_image_url = Field()
